d3aa58657101970f46278c67aad211e991b4c01e
tquean15012003 <tquean15012003@gmail.com>
2024-01-14 15:08:52 +0800

    fix(script): Update comments

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,15 +1,15 @@
-# Get the environment
+# Step 1: Get the environment
 if [ -z "$1" ]; then
   env="dev"
 else
   env=$(awk '{print tolower($0)}' <<< "$1")
 fi
 
-# Replace with your own values
+# Step 2: Specify your aws credentials and app name
 aws_profile="AdministratorAccess-007985056474"
 app="lambda-docker"
 aws_account="007985056474"
 aws_region="ap-southeast-1" 
 
-# Upload the image to ECR
+# Step 3: Run uploading image script
 source ./upload_image.sh $aws_profile $aws_region $env $app $aws_account
